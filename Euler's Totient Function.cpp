43649f7466486a19b39f613571a1e45519d46988
Nur Shuvo <nurshuvo51@gmail.com>
2016-08-24 05:03:36 +0600

    Update Euler's Totient Function.cpp

--- Euler's Totient Function.cpp
+++ Euler's Totient Function.cpp
@@ -1,44 +1,51 @@
 //  * gmail : nurshuvo51@gmail.com
 //  * Implementation of Euler Totient function using Euler's
 //    product Formula
-//  * Formula , phi(n) = n * [ ( 1 - 1/p1 ) * ( 1 - 1/p2 )* ( 1 - 1/p3 ) * ....*( 1 - 1/pn )  ]
-//    where p1,p2,p3...,pn are all prime factors
+//  * Implementation of Euler Totient function using Euler's
+//    product Formula
+//    algorithm :
+//        1) Initialize result as n
+//        2) Consider every number 'p' (where 'p' varies from 2 to √n).
+//        If p divides n, then do following
+//           a) Subtract all multiples of p from 1 to n [all multiples of p
+//           will have gcd more than 1 (at least p) with n]
+//           b) Update n by repeatedly dividing it by p.
+//        3) If the reduced n is more than 1, then remove all multiples
+//        of n from result.
 #include<stdio.h>
-int  Euler_totient(int n)
+int phi(int n)
 {
-    int i,j=0,two;
-    int prime[100]; /* prime factor gulo array ta te rakhbo , sob gulo factor distinct */
-    double result= n;
-    while(n%2==0){
-         two=1;
-//        prime[j++]=2;
-         n/=2;
-    }
-    if(two==1)
-        prime[j++]=2;
-//    n must be odd at this point . So we can skip one element ( i.e = i+=2)
-    for(i=3;i*i<=n;i+=2){
-        while(n%i==0){
-            n/=i;
+    int result = n;   // Initialize result as n
+
+    // Consider all prime factors of n and subtract their
+    // multiples from result
+    for (int p=2; p*p<=n; ++p)
+    {
+        // Check if p is a prime factor.
+        if (n % p == 0)
+        {
+            // If yes, then update n and result
+            while (n % p == 0)
+                n /= p;
+            result -= result / p;
         }
-        prime[j++]=i;
     }
-//    at this point if n is greater than 2 than n itself is a prime factor
-    if(n>2)
-        prime[j++]=n;
 
-     for(i=0;i<j;i++){
-        result*=(1.0-(1.0/prime[i]));
-     }
+    // If n has a prime factor greater than sqrt(n)
+    // (There can be at-most one such prime factor)
+    if (n > 1)
+        result -= result / n;
 
-      return (int)result ;
+    return result;
 }
 int main()
 {
     int n,i,result;
-    for(i=1;i<=10;i++){
-    result= Euler_totient(i);
+
+    for(i=1;i<=100;i++){
+    result= phi(i);
     printf("phi(%d) = %d\n",i,result);
     }
     return 0;
 }
+
